e357e4c6f74f0748117cb55cdd4eedad021b9bda
Scott McKenzie <slammayjammay@gmail.com>
2017-02-02 18:03:26 -0800

    Allow for non-pager output

--- src/index.js
+++ src/index.js
@@ -1,7 +1,18 @@
 #!/usr/bin/env node
 
 const execSync = require('child_process').execSync
+const minimist = require('minimist')
 const pager = require('node-pager')
 
-let log = execSync('git -c color.ui=always log').toString('utf8')
-pager(log)
+let argv = minimist(process.argv.slice(2), {
+  alias: {
+    p: 'pager'
+  }
+})
+
+let options = {
+  pager: argv.pager
+}
+
+let text = execSync('git -c color.ui=always log').toString('utf8')
+options.pager ? pager(text) : console.log(text)
